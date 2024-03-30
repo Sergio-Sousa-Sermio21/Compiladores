caa7b32e56f7e8641151282f7b5c071d8d5322c3
PedroBraz3 <120532361+PedroBraz3@users.noreply.github.com>
2024-03-30 18:16:15 +0000

    Vm

---
+++ src/tVm.java
@@ -0,0 +1,24 @@
+import java.io.DataInputStream;
+import java.io.FileInputStream;
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+
+public class tVm {
+
+    private Array[] globalMemory;
+    private ArrayList<Instrucion> instrucions = new ArrayList<>();
+
+    private ArrayList<Object> constantPool = new ArrayList<>();
+
+    public tVm(String[] args){
+        getFile(args);
+    }
+
+    private void getFile(String[] args){
+        DataInputStream din = new DataInputStream(new FileInputStream("file.dat"));
+    }
+
+    public static void main(String[] args) {
+        tVm Vm = new tVm(args);
+    }
+}
