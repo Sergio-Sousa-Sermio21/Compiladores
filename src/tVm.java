d0862eaa9d9ac1298214988aef8213b15429a92c
PedroBraz3 <120532361+PedroBraz3@users.noreply.github.com>
2024-03-30 18:33:01 +0000

    Configuracion

--- src/tVm.java
+++ src/tVm.java
@@ -1,5 +1,6 @@
 import java.io.DataInputStream;
 import java.io.FileInputStream;
+import java.io.IOException;
 import java.lang.reflect.Array;
 import java.util.ArrayList;
 
@@ -10,15 +11,19 @@ public class tVm {
 
     private ArrayList<Object> constantPool = new ArrayList<>();
 
-    public tVm(String[] args){
+    public tVm(String[] args) throws IOException{
         getFile(args);
     }
 
-    private void getFile(String[] args)throws IOException{
-        DataInputStream din = new DataInputStream(new FileInputStream("file.dat"));
+    private void getFile(String[] args)throws IOException {
+        DataInputStream din = new DataInputStream(new FileInputStream(args[0]));
+        while(din.available()>0){
+            Byte bytes = din.readByte();
+            System.out.println(bytes);
+        }
     }
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         tVm Vm = new tVm(args);
     }
 }
