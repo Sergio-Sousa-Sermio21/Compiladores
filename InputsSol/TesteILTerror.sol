9fe233eb2067bdf825143e24828a512276d1c75f
PedroBraz3 <120532361+PedroBraz3@users.noreply.github.com>
2024-04-14 18:49:08 +0100

    a

--- InputsSol/TesteILTerror.sol
+++ InputsSol/TesteILTerror.sol
@@ -1,6 +1,10 @@
 print 5 >= 5;
 print 4.9 > 5;
+
 print "teste" > 1;
+print "teste" > 10.0;
+print "teste" >= true;
+print "teste" > "dog"
 print true <= false;
+print true > 1.0;
 print 1 < false;
-print 2.0 > "teste";
--- InputsSol/TesteMULTerror.sol
+++ InputsSol/TesteMULTerror.sol
@@ -3,5 +3,5 @@ print "teste" * 2;
 print false * 2;
 print "teste" * "dog";
 print false * true;
-print false * 10;
+print false * 10.0;
 print true * "teste";
