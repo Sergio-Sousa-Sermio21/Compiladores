b238e5e6a071521001918dae0d41ade37b6fe4e7
RenatoPereira1244 <reis.pereira@hotmail.com>
2024-04-28 21:06:02 +0100

    comi

--- InputsParte3/TesteSome.sol
+++ InputsParte3/TesteSome.sol
@@ -1,2 +1,10 @@
-int i = 10, int j = 2;
-print (i + i) * j * i
+int i = 10;
+int j = 2;
+real w = 3.5;
+print (i + i) * j * i;
+print i + i * j * i;
+print i + (i * j) * i;
+print i + i * (j * i);
+print (i + i) * j * i + "ualg" + 100;
+print (i * i) / (j % i) * w;
+print w - i * (i / j) % i + w;
