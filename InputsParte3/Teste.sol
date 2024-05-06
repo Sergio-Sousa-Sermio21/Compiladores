c49dbe2344f86018213c51834525e0a5179f7cc2
PedroBraz3 <120532361+PedroBraz3@users.noreply.github.com>
2024-05-06 09:18:27 +0100

    Testes

--- InputsParte3/Teste.sol
+++ InputsParte3/Teste.sol
@@ -1,22 +1,9 @@
 /*
 * Computes all prime numbers upto n
 */
-int i, j, n = 30;
-bool isPrime;
-string result = "";
-for i = 1 to n do
-begin
-    // check if i is prime
-    isPrime = true;
-        for j = 2 to i/2 do
-            if i % j == 0 then
-            begin
-                isPrime = false;
-                break;
-            end
-    break;
-    if isPrime
-    then result = result + " " + i;
-end
-print "Prime numbers upto " + n + ":" + result;
+bool t = false;
+if t then
+    print "Ola";
+    else
+    print "Adeus";
 
