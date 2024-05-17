02b56f0fc851a2a75ebf6046db4560e9eba006bf
PedroBraz3 <120532361+PedroBraz3@users.noreply.github.com>
2024-05-17 11:42:19 +0100

    TesteStrings

--- inputs4/teste.sol
+++ inputs4/teste.sol
@@ -1,29 +1,9 @@
-void mainnn()
+void main()
 begin
-    int n = fun(1, 2, 3);
-    fun(1, 2);
-    hello("Maria");
-    hello(5);
+    print add("adeus");
 end
 
-void hello(string s)
+string add(string a)
 begin
-    print "Hello" + s;
-end
-
-real zzz(int x)
-begin
-    return x + 1;
-end
-
-bool xpto()
-begin
-    print "ola";
-end
-
-int fun(int x, int y)
-begin
-    bool b = xpto;
-    if (x < 1) then
-        return x + y;
+    return a + " " + "Ola";
 end
